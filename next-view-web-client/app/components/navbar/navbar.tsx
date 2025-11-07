44ce8de7864e4d442bcc4cb5c567b4101be694b1
Cooper Harris <cwharris365@gmail.com>
2025-11-07 15:38:48 -0700

    feat(frontend) set up Next.js project

---
+++ next-view-web-client/app/components/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className='flex justify-between items-center p-[1em]'>
+      <Link href='/' className='cursor-pointer'>
+        <Image
+          src='/next-view-logo.png'
+          alt='Next View Logo'
+          width={90}
+          height={50}
+        />
+      </Link>
+    </nav>
+  );
+}
